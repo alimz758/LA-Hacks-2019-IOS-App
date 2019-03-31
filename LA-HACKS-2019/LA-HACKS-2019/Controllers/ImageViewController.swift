750adfdcd06a78549f4e377a5b3f2e9443f5d6ac
Glarence Zhao <glarencz@usc.edu>
2019-03-30 23:12:32 -0700

    Added photo saving from taking photo, but also saves selected image from gallery

--- LA-HACKS-2019/LA-HACKS-2019/Controllers/ImageViewController.swift
+++ LA-HACKS-2019/LA-HACKS-2019/Controllers/ImageViewController.swift
@@ -22,6 +22,20 @@ class ImageViewController : UIViewController{
         CameraHandler.shared.showActionSheet(vc: self)
         CameraHandler.shared.imagePickedBlock = { (image) in
             /* get your image here */
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
         }
     }
 }
